ea1bc1752257e97b0d20b1d518f4c0db47fc6db8
M Aadil Rajputecho <maadilrajput05@gmail.com>
2026-03-05 12:01:32 +0500

    feature-1 rebase

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ console.log("first commit in feature 2")
 // console.log("main branch")
 // console.log("first commit in main branch")
 
-
+console.log("feature-1 rebase commit will merge with main")
 
 
 app.listen(PORT ,()=>console.log(`server is running on ${PORT}`))
